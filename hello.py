2b224f05687ca77e79138d98ff42a7bb3adb5fac
SantanaBreno <breno.lima@sou.inteli.edu.br>
2024-02-09 20:45:22 -0300

    Mudança na classe(master)

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 class Pessoa():
-    def __init__(self, name):
+    def __init__(self, name, height):
         self.name = name
+        self.height = height
 
     def cumprimentar(self):
-        print(f'Olá {self.name}')
+        print(f'Olá {self.name}!, sua altura é {self.height}')
 
-pessoa = Pessoa('Breno')
+pessoa = Pessoa('Breno', 1.82)
 
 pessoa.cumprimentar()
